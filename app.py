ca36f88d782513f7a09b4cbd056478a5485b46ac
Tomo Ueda <tueda14@gmail.com>
2015-10-19 16:01:16 -0700

    Created generic framework.

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-import json
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
 
-import flask
-import view
-import api
-
-app = flask.Flask(__name__)
-app.register_blueprint(api.app)
-app.register_blueprint(view.app)
-app.debug = True
+@app.route("/")
+def hello():
+    asset_paths = {"css_path": url_for('static', filename='index.css'),
+            "city_image_path": url_for('static', filename='city.png'),
+            "getstarted_image_path": url_for('static', filename='start.png'),
+            "logo_image_path": url_for('static', filename="logo.png")
+            }
+    return render_template('index.html', **asset_paths) 
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
